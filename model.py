7ee159de3b7ad6409f3bd3af3503cfb4aec7ea4d
= <ywc1026@163.com>
2016-10-30 10:20:06 +0800

    first commit

---
+++ model.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+from tensorflow.python.ops import rnn_cell
+from tensorflow.python.ops import seq2seq
+
+import numpy as np
+
+class Model():
+    """Generate the Model"""
+    def __init__(self, args, infer=False):
+        self.args = args
+        if infer:
+            args.batch_size = 1
+            args.seq_length = 1
+
+        if args.model == 'rnn':
+            cell_fn = rnn_cell.BasicRNNCell
+        elif args.model == 'gru':
+            cell_fn = rnn_cell.GRUCell
+        elif args.model == 'lstm':
+            cell_fn = rnn_cell.BasicLSTMCell
+        else:
+            raise Exception("model type not supported: {}".format(args.model))
+
+        cell = cell_fn(args.rnn_size)
+
+        # if is_training:
+        #     cell = tf.nn.rnn_cell.DropoutWrapper(cell, output_keep_prob=0.5)
+
+        self.cell = cell = rnn_cell.MultiRNNCell([cell] * args.num_layers)
+
+        self.input_data = tf.placeholder(tf.float32, [args.batch_size, args.seq_length])
+        self.targets = tf.placeholder(tf.float32, [args.batch_size, args.seq_length])
+        self.initial_state = cell.zero_state(args.batch_size, tf.float32)
+
+        softmax_w = tf.get_variable("softmax_w", [args.rnn_size, 1])
+        softmax_b = tf.get_variable("softmax_b", [1])
+ 
+        iw = tf.get_variable("input_w", [1,args.rnn_size])
+        ib = tf.get_variable("input_b", [args.rnn_size])
+        inputs = [(tf.matmul(inputs_, iw)+ib) for inputs_ in tf.split(1, args.seq_length, self.input_data)]
+
+        # if is_training:
+        #     inputs = [tf.nn.dropout(input_, 0.5) for input_ in inputs]
+
+        outputs, state = tf.nn.rnn(cell, inputs, initial_state=self.initial_state)
+        rnn_output = tf.reshape(tf.concat(1, outputs), [-1, args.rnn_size])
+        logits = tf.matmul(rnn_output, softmax_w) + softmax_b
+        self.cost = tf.reduce_mean(tf.square(logits - tf.reshape(self.targets, [-1])))
+        self.final_state = state
+        self.lr = tf.Variable(0.0, trainable=False)
+        tvars = tf.trainable_variables()
+        grads, _ = tf.clip_by_global_norm(tf.gradients(self.cost, tvars),
+                args.grad_clip)
+        optimizer = tf.train.AdamOptimizer(self.lr)
+        self.train_op = optimizer.apply_gradients(zip(grads, tvars))
+
+
+
+
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import numpy as np
+import os
+from datetime import time
+
+
+class DataLoader():
+    """Create a new dataframe of raw data """
+    def __init__(self, data_dir, batch_size, num_steps):
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.num_steps = num_steps
+
+        file = os.path.join(data_dir, "stkhf2013_000026sz.csv")
+        self.preprocess(file)
+        self.create_batches()
+        self.reset_batch_pointer()
+
+
+    def preprocess(self, file):
+        reader = pd.read_csv(file, encoding='gbk', iterator=True)
+        df = reader.get_chunk(200000)
+        df.index = pd.DatetimeIndex(df['QTime'])
+        pct2 = df['ChangePCT2'].between_time(time(9,30), time(15,0))
+        pct = pct2.reset_index()['ChangePCT2']
+        self.pct = np.array(pct)
+
+    def create_batches(self):
+        self.num_batches = int(self.pct.size / (self.batch_size * self.num_steps))
+        self.pct = self.pct[:self.num_batches * self.batch_size * self.num_steps]
+        xdata = self.pct
+        ydata = np.copy(self.pct)
+        ydata[:-1] = xdata[1:]
+        ydata[-1] = xdata[0]
+        self.x_batches = np.split(xdata.reshape(self.batch_size, -1), self.num_batches, 1)
+        self.y_batches = np.split(ydata.reshape(self.batch_size, -1), self.num_batches, 1)
+
+
+    def next_batch(self):
+        x, y = self.x_batches[self.pointer], self.y_batches[self.pointer]
+        self.pointer += 1
+        return x, y
+
+
+    def reset_batch_pointer(self):
+        self.pointer = 0
+
+
+
+
+
+
+
+
+
+
